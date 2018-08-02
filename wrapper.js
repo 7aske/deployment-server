9cfc1cd4fefbc995cbe2c96823eb737dec5df02a
Nikola Tasic <ntasic7@gmail.com>
2018-08-02 14:24:02 +0200

    fixed ommited env setup for server

--- wrapper.js
+++ wrapper.js
@@ -1,7 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const child_process = require("child_process");
-const express = require("express");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const child_process = require('child_process');
+const express = require('express');
 const wrapper = express();
 const router = express.Router();
 wrapper.use('/', router);
@@ -11,46 +11,43 @@ let server = child_process.execFile('node', ['server.js'], { env: { PORT: server
 server.stdout.pipe(process.stdout);
 server.stderr.pipe(process.stdout);
 function formatStdOut(stdout, response) {
-    //format stdout to differentiate between errors and messages
-    const data = stdout.toString();
-    if (data.indexOf('fatal') != -1 || data.indexOf('ERR') != -1 || data.indexOf('error') != -1) {
-        response.errors.push(data);
-    }
-    else {
-        response.messages.push(data);
-    }
-    return response;
+	//format stdout to differentiate between errors and messages
+	const data = stdout.toString();
+	if (data.indexOf('fatal') != -1 || data.indexOf('ERR') != -1 || data.indexOf('error') != -1) {
+		response.errors.push(data);
+	} else {
+		response.messages.push(data);
+	}
+	return response;
 }
 router.get('/', (req, res) => {
-    res.send('Wrapper server');
+	res.send('Wrapper server');
 });
 router.post('/', (req, res) => {
-    server.kill();
-    const git = child_process.execFile('git', ['pull'], { env: { PORT: serverPORT } });
-    let response = {
-        messages: [],
-        errors: []
-    };
-    git.stderr.on('data', data => {
-        response = formatStdOut(data, response);
-    });
-    git.stdout.on('data', data => {
-        response = formatStdOut(data, response);
-    });
-    git.on('close', code => {
-        if (process.env.NODE_ENV == 'dev')
-            console.log('Git process exited with code', code);
-        if (code == 0 && response.errors.length == 0) {
-            server = child_process.execFile('node', ['server.js']);
-            server.stdout.pipe(process.stdout);
-            server.stderr.pipe(process.stdout);
-            res.send(response);
-        }
-        else {
-            res.send(response);
-        }
-    });
+	server.kill();
+	const git = child_process.execFile('git', ['pull'], { env: { PORT: serverPORT } });
+	let response = {
+		messages: [],
+		errors: []
+	};
+	git.stderr.on('data', data => {
+		response = formatStdOut(data, response);
+	});
+	git.stdout.on('data', data => {
+		response = formatStdOut(data, response);
+	});
+	git.on('close', code => {
+		if (process.env.NODE_ENV == 'dev') console.log('Git process exited with code', code);
+		if (code == 0 && response.errors.length == 0) {
+			server = child_process.execFile('node', ['server.js']);
+			server.stdout.pipe(process.stdout);
+			server.stderr.pipe(process.stdout);
+			res.send(response);
+		} else {
+			res.send(response);
+		}
+	});
 });
 wrapper.listen(PORT, () => {
-    console.log('Wrapper running on port ' + PORT);
+	console.log('Wrapper running on port ' + PORT);
 });
