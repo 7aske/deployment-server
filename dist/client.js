f990ce3009f650fdf59df94cad98d69ba52540ea
Nikola Tasic <ntasic7@gmail.com>
2020-01-14 21:07:45 +0100

    rm dist

--- dist/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var path_1 = require("path");
-var client = express_1.Router();
-client.get("/", function (req, res) {
-    res.sendFile(path_1.join(process.cwd(), "dist/client/dist/renderer/views/renderer.html"));
-});
-exports.default = client;
--- dist/middleware/https-redirect.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function httpsRedirect(req, res, next) {
-    if (req.protocol == "http")
-        res.status(302).redirect("https://" + req.headers.host + req.url);
-    else
-        next();
-}
-exports.httpsRedirect = httpsRedirect;
